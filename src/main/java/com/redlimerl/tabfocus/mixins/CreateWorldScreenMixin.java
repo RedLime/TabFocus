bef81a23d6bee4f30eb968feb0e94aa60155afbe
RedLime <redlimerl@gmail.com>
2022-01-13 12:17:59 +0900

    Fixed seed input box issue (#1)

--- src/main/java/com/redlimerl/tabfocus/mixins/CreateWorldScreenMixin.java
+++ src/main/java/com/redlimerl/tabfocus/mixins/CreateWorldScreenMixin.java
@@ -2,6 +2,7 @@ package com.redlimerl.tabfocus.mixins;
 
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +17,7 @@ public abstract class CreateWorldScreenMixin {
 
     @Redirect(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/world/CreateWorldScreen;buttonClicked(Lnet/minecraft/client/gui/widget/ButtonWidget;)V"))
     public void buttonClickedRedirect(CreateWorldScreen instance, ButtonWidget button) {
-        if (FOCUSED_BUTTON_ORDER == -1 && drawableHelpers.size() > 0) {
+        if (drawableHelpers.size() > 0 && (FOCUSED_BUTTON_ORDER == -1 || drawableHelpers.get(FOCUSED_BUTTON_ORDER) instanceof TextFieldWidget)) {
             this.buttonClicked(button);
         }
     }
