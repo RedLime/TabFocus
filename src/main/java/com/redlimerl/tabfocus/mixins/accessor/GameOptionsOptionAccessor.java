4e92bdfc08806cc2f25b745690ca5442a31de905
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2022-02-06 18:25:49 +0100

    1.8.0

--- src/main/java/com/redlimerl/tabfocus/mixins/accessor/GameOptionsOptionAccessor.java
+++ src/main/java/com/redlimerl/tabfocus/mixins/accessor/GameOptionsOptionAccessor.java
@@ -1,14 +1,15 @@
 package com.redlimerl.tabfocus.mixins.accessor;
 
+import net.minecraft.client.options.GameOption;
 import net.minecraft.client.options.GameOptions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(GameOptions.Option.class)
+@Mixin(GameOption.class)
 public interface GameOptionsOptionAccessor {
-    @Accessor("step")
+    @Accessor("field_7688")
     float getStep();
 
-    @Accessor("min")
+    @Accessor("field_7689")
     float getMin();
 }
