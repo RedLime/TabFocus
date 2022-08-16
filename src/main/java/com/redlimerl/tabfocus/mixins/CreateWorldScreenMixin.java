c1c30d6087b7e3cb8f291ac30ea808f33840fe47
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-08-17 01:11:35 +0200

    3 fixes
    fixed sound sliders not working
    fixed keybind buttons not working
    fixed customize world type button in more options in createworldscreen not working

--- src/main/java/com/redlimerl/tabfocus/mixins/CreateWorldScreenMixin.java
+++ src/main/java/com/redlimerl/tabfocus/mixins/CreateWorldScreenMixin.java
@@ -3,7 +3,6 @@ package com.redlimerl.tabfocus.mixins;
 import com.redlimerl.tabfocus.FocusableWidget;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +16,7 @@ public abstract class CreateWorldScreenMixin {
 
     @Redirect(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/world/CreateWorldScreen;buttonClicked(Lnet/minecraft/client/gui/widget/ButtonWidget;)V"))
     public void buttonClickedRedirect(CreateWorldScreen instance, ButtonWidget button) {
-        if (MinecraftClient.getInstance().currentScreen instanceof SelectWorldScreen) return;
+        if (!(MinecraftClient.getInstance().currentScreen instanceof CreateWorldScreen)) return;
         if (FocusableWidget.FOCUSED_WIDGET != null) {
             if (FocusableWidget.FOCUSED_WIDGET.isEquals(button) || FocusableWidget.FOCUSED_WIDGET.is(TextFieldWidget.class)) {
                 this.buttonClicked(button);
